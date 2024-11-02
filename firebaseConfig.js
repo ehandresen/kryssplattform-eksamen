9702e56ef6ac439d6d324a35f28af61e4f4ad55f
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-11-02 17:15:15 +0100

    initialize firebase and firebase auth

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+// we could possibly move this to an 'firebaseEnv.js' file and import it in
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRXc4SXaRCHbbJKNGBl_8KtDdQjODmWeA',
+  authDomain: 'kryss-exam.firebaseapp.com',
+  projectId: 'kryss-exam',
+  storageBucket: 'kryss-exam.firebasestorage.app',
+  messagingSenderId: '804001097958',
+  appId: '1:804001097958:web:287cd47a9d6ce3cf5eb327',
+};
+
+// initialize firebase
+const app = initializeApp(firebaseConfig);
+
+// initialize firebase authentication
+export const auth = getAuth(app);
