fa84d6b36cd29c05b6646f35999e7b1f296e2372
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-11-09 23:17:55 +0100

    add functionality to like an artwork

--- components/gallery/ArtworkCard.tsx
+++ components/gallery/ArtworkCard.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import { Artwork } from "../../types/artwork";
 
 interface ArtworkCardProps {
   artwork: Artwork;
+  isLiked: boolean;
+  numLikes: number;
+  toggleLike: () => void;
 }
 
-export default function ArtworkCard({ artwork }: ArtworkCardProps) {
+export default function ArtworkCard({
+  artwork,
+  isLiked,
+  numLikes,
+  toggleLike,
+}: ArtworkCardProps) {
   return (
     <View className="p-4 bg-white rounded-lg shadow-md mb-4">
       <Image
@@ -17,6 +25,16 @@ export default function ArtworkCard({ artwork }: ArtworkCardProps) {
       <Text className="text-lg font-bold mb-1">{artwork.title}</Text>
       <Text className="text-sm text-gray-600 mb-2">by {artwork.artistId}</Text>
       <Text className="text-sm text-gray-700">{artwork.description}</Text>
+
+      <Text>
+        {numLikes} {numLikes === 1 ? "like" : "likes"}
+      </Text>
+
+      <Pressable onPress={toggleLike} style={{ marginTop: 10 }}>
+        <Text style={{ color: isLiked ? "blue" : "gray" }}>
+          {isLiked ? "Unlike" : "Like"}
+        </Text>
+      </Pressable>
     </View>
   );
 }
